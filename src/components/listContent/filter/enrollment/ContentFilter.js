3fe5191b31555f4050afb9b679328271cc17f142
EdsonNhancale <edsonnhancale97@gmail.com>
2023-01-30 10:02:21 +0200

    adding new types of fields

--- src/components/listContent/filter/enrollment/ContentFilter.js
+++ src/components/listContent/filter/enrollment/ContentFilter.js
@@ -14,6 +14,7 @@ import {
     KeyboardDatePicker,
 } from '@material-ui/pickers';
 import { format } from 'date-fns';
+import SelectBottom from '../../../selectBottom/SelectBottom.js'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -80,66 +81,9 @@ function ContentFilter({ headers, type }) {
         <div style={{ display: "flex", flexWrap: "wrap", alignItems: "center", marginBottom: 10 }}>
             {
                 localFilters.map((colums, index) => (
-                    colums.valueType === "List" ?
-                        <div key={index} style={{ margin: "0 5px" }}>
-                            <small style={{ fontSize: 9 }}>{" "} <br /> </small>
-                            <FormControl variant="outlined" size="small" className={classes.formControl} style={{ marginTop: -18 }}>
-                                <InputLabel id="demo-simple-select-label">{colums.header}</InputLabel>
-                                <Select
-                                    style={{ width: 200 }}
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={filtersValues[colums?.id]}
-                                    onChange={(e) => {
-                                        onChangeFilters(e, colums.id);
-                                        setfiltersValues(prevState => ({ ...prevState, [colums.id]: e?.target?.value }))
-                                    }}
-                                    className="mr-1 filter-input-select"
-                                    placeholder={colums.header} key={index}
-                                >
-                                    {colums.optionSets.map((x, li) => <MenuItem key={li} value={x.code}>{x.displayName}</MenuItem>)}
-                                </Select>
-                            </FormControl>
-                        </div>
-                        : colums.valueType === "DATE" ?
-                            <div style={{ margin: "0 5px" }}>
-                                <MuiPickersUtilsProvider utils={DateFnsUtils} >
-                                    <KeyboardDatePicker
-                                        style={{ width: 200, marginTop: 7 }}
-                                        variant="inline"
-                                        inputVariant="outlined"
-                                        format="yyyy/MM/dd"
-                                        margin="normal"
-                                        id="date-picker-inline"
-                                        label={colums.header}
-                                        value={filtersValues[colums?.id] && format(filtersValues[colums?.id], "yyyy/MM/dd")}
-                                        onChange={(e) => onChangeFilters({ target: { value: e } }, colums.id)}
-                                        KeyboardButtonProps={{
-                                            'aria-label': 'change date',
-                                        }}
-                                        size="small"
-                                    />
-                                </MuiPickersUtilsProvider>
-                            </div>
-                            :
-                            <div key={index} style={{ margin: "0 5px" }}>
-                                <TextField small
-                                    style={{ width: 200 }}
-                                    value={filtersValues[colums?.id] || ""}
-                                    key={index}
-                                    name={colums.id}
-                                    onChange={(e) => onChangeFilters(e, colums.id)}
-                                    className="mr-1 filter-input"
-                                    valueType={getValueType(colums.valueType)}
-                                    type={getValueType(colums.valueType)}
-                                    label={colums.header}
-                                    variant="outlined"
-                                    size="small"
-                                />
-                            </div>
+                    <SelectBottom key={index} title={colums.header} />
                 ))
             }
-
             <div style={{ marginTop: 0 }}>
                 {headers.filter(x => !localFilters.includes(x)).length > 0 &&
                     <Button style={{
