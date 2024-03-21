3ac2933c168c7e1692f101c663783d7810f8a8de
EdsonNhancale <edsonnhancale97@gmail.com>
2024-03-21 10:46:21 +0200

    refactor: updated get2AttributesTei

--- src/utils/commons/get2AttributeTei.js
+++ src/utils/commons/get2AttributeTei.js
@@ -1,8 +1,8 @@
 
 export function get2AttributeTei(tei, program) {
-    const id1 = program.trackedEntityType?.trackedEntityTypeAttributes?.[0]?.trackedEntityAttribute?.id || program.programTrackedEntityAttributes?.[0]?.trackedEntityAttribute?.id || "Variable not difined"
+    const id1 = program.trackedEntityType?.trackedEntityTypeAttributes?.[0]?.trackedEntityAttribute?.id || program.programTrackedEntityAttributes?.[0]?.trackedEntityAttribute?.id || "Variable not defined"
     const id1Name = program.trackedEntityType?.trackedEntityTypeAttributes?.[0]?.trackedEntityAttribute?.displayName || program.programTrackedEntityAttributes?.[0]?.trackedEntityAttribute?.displayName 
-    const id2 = program.trackedEntityType?.trackedEntityTypeAttributes?.[1]?.trackedEntityAttribute?.id || program.programTrackedEntityAttributes?.[1]?.trackedEntityAttribute?.id || "Variable not difined"
+    const id2 = program.trackedEntityType?.trackedEntityTypeAttributes?.[1]?.trackedEntityAttribute?.id || program.programTrackedEntityAttributes?.[1]?.trackedEntityAttribute?.id || "Variable not defined"
     const id2Name = program.trackedEntityType?.trackedEntityTypeAttributes?.[1]?.trackedEntityAttribute?.displayName || program.programTrackedEntityAttributes?.[1]?.trackedEntityAttribute?.displayName 
 
     return (`${id1Name}: ${tei?.[id1] || "---"};${id2Name}: ${tei?.[id2] || "---"}`)
